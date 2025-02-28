bcfeb50da710b26afe7e69a27d5f4f778375770f
Emma <emmalanza@hotmail.com>
2025-02-28 18:18:57 +0100

    add netlify toml

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from 'astro/config';
 import htmx from 'astro-htmx';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), htmx(), react()],
-  output: 'static',
+  output: "server", // 🔥 Necesario para usar SSR
+  adapter: netlify({}),
 });
